b0a1b9c326b5d8da613432823120961b83c7d962
Yash Kumar <yash299@hotmail.com>
2026-09-18 00:41:51 +0530

    Time: 3 ms (18.20%) | Memory: 9.6 MB (10.85%) - LeetSync

--- 851-goat-latin/goat-latin.cpp
+++ 851-goat-latin/goat-latin.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
     string toGoatLatin(string sentence) {
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        stringstream ss(sentence);
-        string word;
-        string result = "";
-        int word_count = 1;
+        std::stringstream ss(sentence);
+        std::string temp_word;
+        std::vector<std::string> words;
 
-        while (ss >> word) {
-            char first_letter = word[0];
-            
-            if (vowels.count(first_letter)) {
-                word += "ma";
-            } else {
-                word = word.substr(1) + first_letter + "ma";
-            }
-            
-            word += string(word_count, 'a');
-            
-            if (result.empty()) {
-                result += word;
-            } else {
-                result += " " + word;
-            }
-            
-            word_count++;
+    // Store all words into the vector
+        while (ss >> temp_word) {
+            words.push_back(temp_word);
         }
-        
-        return result;
+        string sen ;
+        vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (int i = 0 ; i < words.size();i++ ){
+            string temp = words[i];
+            if (find(vowels.begin(), vowels.end(), temp[0]) != vowels.end()){
+                if (i ==words.size()-1){
+                    temp+="ma"+ string(i+1, 'a');
+                    sen+=temp;
+                }
+                else{
+                    temp+="ma"+ string(i+1, 'a')+ " ";
+                    sen+=temp;}
+            }
+            else{
+                char temp1= temp[0];
+                temp.erase(0, 1);
+                if (i ==words.size()-1){
+                    temp += temp1 + string("ma") + string(i + 1, 'a');
+                    sen+=temp;
+                }
+                else{
+                    temp += temp1 + string("ma") + string(i + 1, 'a')+" ";
+                    sen+=temp;
+                }
+                
+            }         
+      
+
+            
+        } 
+        return sen;
     }
+       
 };
