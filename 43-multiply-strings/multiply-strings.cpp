f8263d6185637b825b938bb1677bf37f1c2b3969
Yash Kumar <yash299@hotmail.com>
2026-09-18 23:46:50 +0530

    Time: 3 ms (55.71%) | Memory: 9.5 MB (72.01%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,4 +1,5 @@
-
+#include <string>
+#include <vector>
 
 class Solution {
 public:
@@ -7,27 +8,25 @@ public:
         
         int m = num1.size();
         int n = num2.size();
-        std::unordered_map<int, int> res_map;
+        std::vector<int> result(m + n, 0);
         
-        for (int i = m - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
                 int mul = (num1[i] - '0') * (num2[j] - '0');
-                int p1 = i + j;
-                int p2 = i + j + 1;
+                int sum = mul + result[i + j + 1];
                 
-                int sum = mul + res_map[p2];
-                res_map[p2] = sum % 10;
-                res_map[p1] += sum / 10;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
             }
         }
         
-        std::string res_str = "";
-        for (int idx = 0; idx < m + n; ++idx) {
-            if (!(res_str.empty() && res_map[idx] == 0)) {
-                res_str += std::to_string(res_map[idx]);
+        std::string productStr = "";
+        for (int num : result) {
+            if (!(productStr.empty() && num == 0)) {
+                productStr.push_back(num + '0');
             }
         }
         
-        return res_str.empty() ? "0" : res_str;
+        return productStr.empty() ? "0" : productStr;
     }
 };
