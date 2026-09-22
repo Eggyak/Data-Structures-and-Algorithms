ee3e4741c7cb749bc516bc7c8f83a0deab84096b
Yash Kumar <yash299@hotmail.com>
2026-09-22 23:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (79.62%) - LeetSync

---
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestInteger(int n, int s) {
+        if (s < 0 || s > 9 * n) {
+            return -1;
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int digit = min(9, s);
+            result = result * 10 + digit;
+            s -= digit;
+        }
+
+        return result;
+    }
+};
